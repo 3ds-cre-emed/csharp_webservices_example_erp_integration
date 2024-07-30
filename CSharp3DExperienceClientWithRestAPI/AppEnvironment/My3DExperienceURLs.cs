8ae174b63befa4a8bda72eca219be983a1f40662
Selimhan Bek <73447706+SelimhanBek@users.noreply.github.com>
2024-07-30 12:24:44 +0300

    Update My3DExperienceURLs.cs

--- CSharp3DExperienceClientWithRestAPI/AppEnvironment/My3DExperienceURLs.cs
+++ CSharp3DExperienceClientWithRestAPI/AppEnvironment/My3DExperienceURLs.cs
@@ -5,12 +5,12 @@ namespace CSharp3DExperienceClientWithRestAPI.AppEnvironment
     class My3DExperienceURLs
     {
         /* My Platform Tenant ID */
-        public String MyTenant => "R1132101096635";
+        public String MyTenant => "<RXXXXXXXYOURTENANT>";
 
         /* 3D Passport URL */
-        public String My3DPassport => "https://r1132101096635-eu1.iam.3dexperience.3ds.com";
+        public String My3DPassport => "https://<rXXXXXYOURTENANT>-eu1.iam.3dexperience.3ds.com";
 
         /* 3D Sapce URL */
-        public String My3DSpace => "https://r1132101096635-eu1-space.3dexperience.3ds.com/enovia";
+        public String My3DSpace => "https://<rXXXXXXYOURTENANT>-eu1-space.3dexperience.3ds.com/enovia";
     }
 }
